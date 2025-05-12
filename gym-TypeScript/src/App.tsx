8fceb53c1d85542eb0fd239d0fc477d64975ab75
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-12 19:50:37 +0630

    set up Tailwind CSS for styling

---
+++ gym-TypeScript/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl underline font-bold bg-amber-50' >
+
+      Hello Word
+      </h1>
+        
+    </>
+  )
+}
+
+export default App
