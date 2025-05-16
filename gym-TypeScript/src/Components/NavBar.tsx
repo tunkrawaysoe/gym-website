c49a9889034c6557dd02c69e061cb8d47d18e51d
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-16 14:33:20 +0630

    implement responsive NavBar with smooth scroll links and active state handling

--- gym-TypeScript/src/Components/NavBar.tsx
+++ gym-TypeScript/src/Components/NavBar.tsx
@@ -1,24 +1,55 @@
+import { useState } from 'react';
+import Logo from '@/assets/logo.png';
+import Link from '@/Components/Link';
 
-
-type Props = {}
+type Props = {};
 
 const NavBar = (props: Props) => {
-  const flexBetween = "flex items-center w-full justify-between";
+  const [selectedPage, setSelectedPage] = useState("home");
+  const flexBetween = "flex items-center justify-between";
 
   return (
-    <div className={`${flexBetween} py-2 fixed z-30`}>
-      <div className={`${flexBetween} mx-auto w-5/6 bg-black gap-3`}>
-        <div>
-          Hello
+    <nav className={`${flexBetween} py-2 w-full fixed z-30 bg-white shadow`}>
+      <div className={`${flexBetween} mx-auto w-5/6`}>
+        <div className={`${flexBetween} w-full gap-16`}>
+          {/* Left side */}
+          <img alt="logo" src={Logo} className="h-6" />
 
+          {/* Right side */}
+          <div className={`${flexBetween} w-full`}>
+            <div className={`${flexBetween} gap-8`}>
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Benefits"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Our Classes"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact Us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+            <div className={`${flexBetween} gap-8`}>
+              <p className="cursor-pointer hover:text-primary-500">Sign In</p>
+              <button className="rounded-md bg-primary-500 px-4 py-2 text-white hover:bg-primary-300 transition duration-300">
+                Become a Member
+              </button>
+            </div>
+          </div>
         </div>
-        <div>
-          World
-        </div>
-
       </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
