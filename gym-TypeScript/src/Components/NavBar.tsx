d8b5034595d8c21afbe1fd3ad3f6075ea47dc399
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-17 14:38:51 +0630

    add responsive NavBar with logo, navigation links, Sign In, and Become a Member button. Implement mobile menu toggle using Heroicons.

--- gym-TypeScript/src/Components/NavBar.tsx
+++ gym-TypeScript/src/Components/NavBar.tsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
 import Logo from '@/assets/logo.png';
 import Link from '@/Components/Link';
 import { SelectedPage } from '@/Shared/type';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { useState } from 'react';
+import { Bars3Icon,XMarkIcon } from '@heroicons/react/16/solid';
+import ActionButton from '@/Shared/ActionButton';
+
 
 
 type Props = {
@@ -11,48 +15,98 @@ type Props = {
 };
 
 const NavBar = ({selectedPage,setSelectedPage}: Props) => {
+  const [isMenuToggled,setIsMenuToggled] = useState<boolean>(false)
   const flexBetween = "flex items-center justify-between";
+  const isNonMobileScreen = useMediaQuery("(min-width: 1060px)");
+ return (
+    <nav>
+      <div
+        className={` ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img alt="logo" src={Logo} />
 
-  return (
-    <nav className={`${flexBetween} py-2 w-full fixed z-30 bg-white shadow`}>
-      <div className={`${flexBetween} mx-auto w-5/6`}>
-        <div className={`${flexBetween} w-full gap-16`}>
-          {/* Left side */}
-          <img alt="logo" src={Logo} className="h-6" />
-
-          {/* Right side */}
-          <div className={`${flexBetween} w-full`}>
-            <div className={`${flexBetween} gap-8`}>
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Benefits"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact Us"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-            </div>
-            <div className={`${flexBetween} gap-8`}>
-              <p className="cursor-pointer hover:text-primary-500">Sign In</p>
-              <button className="rounded-md bg-primary-500 px-4 py-2 text-white hover:bg-primary-300 transition duration-300">
-                Become a Member
+            {/* RIGHT SIDE */}
+            {isNonMobileScreen ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
               </button>
-            </div>
+            )}
           </div>
         </div>
       </div>
+
+      {/* MOBILE MENU MODAL */}
+      {!isNonMobileScreen && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
