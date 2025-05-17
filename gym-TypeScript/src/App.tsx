339f3be60a607ce8de40a07f3777e3a2c5e60a7d
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-17 20:29:58 +0630

    Correct cleanup of scroll event listener in useEffect

--- gym-TypeScript/src/App.tsx
+++ gym-TypeScript/src/App.tsx
@@ -1,13 +1,29 @@
 import NavBar from "@/Scenes/NavBar/NavBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/Shared/type";
-
-
 
 function App() {
   const [selectedPage,setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
-  
+  const [isTopOfPage,setIsTopOfPage] = useState<boolean>(true)
 
+
+  useEffect(() => {
+  const handleScroll = () => {
+    if (window.scrollY === 0) {
+      setIsTopOfPage(true);
+      setSelectedPage(SelectedPage.Home);
+    } else {
+      setIsTopOfPage(false);
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => window.removeEventListener('scroll', handleScroll);
+}, []);
+
+
+  
   return (
     <div className="app bg-gray-50">
       <NavBar selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}/>
