b642f8fbe54447d1d30aeb22b7616f427206d80b
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-18 16:56:33 +0630

    add Home section with responsive design, hero content, and sponsor logos

--- gym-TypeScript/src/Scenes/home/Index.tsx
+++ gym-TypeScript/src/Scenes/home/Index.tsx
@@ -1,6 +1,9 @@
 import HomePageText from '@/assets/HomePageText.png';
 import HomePageGraphic from '@/assets/HomePageGraphic.png'
+import SponserRedBull from '@/assets/SponsorRedBull.png'
 import ActionButton from '@/Shared/ActionButton';
+import SponsorForbes from '@/assets/SponsorForbes.png'
+import SponsorFortune from '@/assets/SponsorFortune.png'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { SelectedPage } from '@/Shared/type';
 import useMediaQuery from '@/hooks/useMediaQuery';
@@ -12,47 +15,42 @@ type Props = {
 }
 
 const Home = ({setSelectedPage}: Props) => {
-    
-    const isNonMobileScreen = useMediaQuery("(min-width: 1060px)");
+  const isNonMobileScreen = useMediaQuery("(min-width: 1060px)");
+
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      
+        <div className='md:flex mx-auto w-5/6 align-center justify-center md:h-5/6'>
         {/* MAIN HEADER */}
-        <div className="z-10 mt-32 md:basis-3/5">
-          {/* HEADINGS */}
-            <div className="relative">
-              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
-                <img alt="home-page-text" src={HomePageText} />
+          <div className='mt-16 md:basis-3/5 '>
+            {/**Headers */}
+            <div className='md:-mt-1'>
+              <div>
+                <img alt='Home-page-text' src={`${HomePageText}`}/>
               </div>
-            </div>
-
-            <p className="mt-8 text-sm">
+              <p>
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
-            </p>
-        
-             {/* ACTIONS */}
-          
-            <ActionButton setSelectedPage={setSelectedPage}>
-              Join Now
-            </ActionButton>
-            <AnchorLink
-              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              </p>
+            </div>
+            {/** Actions */}
+            <div>
+              <ActionButton>
+                Join Now
+              </ActionButton>
+              <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
               href={`#${SelectedPage.ContactUs}`}
-            >
-              <p>Learn More</p>
-            </AnchorLink>
-        </div>
+              >
+                <p>Learn More</p>
+              </AnchorLink>
+            </div>
+          </div>
 
-        {/* IMAGE */}
-        <div
-          className="flex basis-3/5 justify-center md:z-10
-              md:ml-40 md:mt-16 md:justify-items-end"
-        >
-          <img alt="home-pageGraphic" src={HomePageGraphic} />
+          <div>
+            <img alt='Home-page-graphic' src={`${HomePageGraphic}`}/>
+          </div>
         </div>
      
 
@@ -60,10 +58,10 @@ const Home = ({setSelectedPage}: Props) => {
       {isNonMobileScreen && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
-            <div className="flex w-3/5 items-center justify-between gap-8">
-            {/* //   <img alt="redbull-sponsor" src={SponsorRedBull} />
-            //   <img alt="forbes-sponsor" src={SponsorForbes} />
-            //   <img alt="fortune-sponsor" src={SponsorFortune} /> */}
+            <div className="flex  items-center justify-between gap-8">
+            <img alt="redbull-sponsor" src={SponserRedBull} />
+            <img alt="forbes-sponsor" src={SponsorForbes} />
+            <img alt="fortune-sponsor" src={SponsorFortune} />
             </div>
           </div>
         </div>
