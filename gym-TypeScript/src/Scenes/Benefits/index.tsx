af2e030521c0c54b2367e4c057c8b35d2f75d070
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-19 13:48:35 +0630

    Create Header for benefits page

--- gym-TypeScript/src/Scenes/Benefits/index.tsx
+++ gym-TypeScript/src/Scenes/Benefits/index.tsx
@@ -1,4 +1,6 @@
 import { SelectedPage } from "@/Shared/type";
+import Header from "@/Shared/Head";
+
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -6,7 +8,20 @@ type Props = {
 
 const Benefits = ({ setSelectedPage }: Props) => {
   return (
-    <div></div>
+    <section className="mx-auto w-5/6 py-20 h-full">
+      {/**Header */}
+      <div className=" md:w-3/5 md:my-5">
+        <Header>
+          More Than Just Gym.
+        </Header>
+        <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+        </p>
+      </div>
+
+    </section>
   );
 };
 
---
+++ gym-TypeScript/src/Shared/Head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+  children : React.ReactNode,
+}
+
+const Head = ({children}: Props) => {
+  return (
+    <h1 className='text-3xl font-bold font-montserrat '>{children}</h1>
+  )
+}
+
+export default Head
