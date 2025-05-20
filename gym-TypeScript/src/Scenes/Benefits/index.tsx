9e8fbd4c54fdad8758f1839833398fb72e1303d2
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-20 11:53:38 +0630

    Add illustrative image and supportive description text

--- gym-TypeScript/src/Scenes/Benefits/index.tsx
+++ gym-TypeScript/src/Scenes/Benefits/index.tsx
@@ -4,7 +4,8 @@ import { HomeModernIcon,UserGroupIcon,AcademicCapIcon } from "@heroicons/react/1
 import Benefit from "./Benefit";
 import Head from "@/Shared/Head";
 import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
-
+import ActionButton from "@/Shared/ActionButton";
+ActionButton
 
 
 type Props = {
@@ -60,7 +61,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
       </div>
 
       {/** Image and description */}
-      <div className="md:flex mt-6 md:mt-30 justify-between items-center gap-20">
+      <div className="md:flex mt-6  mx-auto md:mt-30 justify-between items-center gap-20">
         {/** Image */}
         <img src={`${BenefitsPageGraphic}`}/>
 
@@ -68,15 +69,35 @@ const Benefits = ({ setSelectedPage }: Props) => {
         <div className="mt-4">
           {/**Title */}
           <div className="relative">
-            <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
-             <Head> MILLIONS OF HAPPY MEMBERS GETTING{" "}</Head>
+            <div className="before:absolute before:-top-16 before:-left-20 before:z-[-1] before:content-abstractwaves">
+             <Head> MILLIONS OF HAPPY MEMBERS GETTING{" "}<span className="text-primary-500">Fit.</span></Head>
             </div>
-
           </div>
 
           {/**Description */}
+          <div className="text-sm md:text-lg mb-6 z-[-1]">
+            <p className="my-5">Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.</p>
+            <p>
+               Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+            </p>
+          </div>
 
           {/**Button */}
+          
+            <div className="relative">
+              <div className=" before:absolute before:-bottom-20 before:right-40 before:content-sparkles">
+                <ActionButton >Join Now</ActionButton>
+
+              </div>
+            </div>
 
         </div>
 
